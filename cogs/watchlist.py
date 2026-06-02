a8a04bc57102394e205c6130895b5cfb91368b2d
Abdallah Abd Elsalam <116673393+Abdallah1959@users.noreply.github.com>
2026-06-03 02:35:37 +0300

    Update watchlist.py

--- cogs/watchlist.py
+++ cogs/watchlist.py
@@ -1,1 +1,8 @@
+from discord.ext import commands
 
+class Watchlist(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+async def setup(bot):
+    await bot.add_cog(Watchlist(bot))
